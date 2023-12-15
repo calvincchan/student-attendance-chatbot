707fc51f9326d0e59f9a054bfe86d25786b5f43c
Calvin C Chan <hello@calvin-c.com>
2023-12-15 15:52:18 -0800

    chore: init commit, load students.csv and ask question about it

---
+++ index.ts
@@ -0,0 +1,60 @@
+/**
+ * To run:
+ *
+ * 1. yarn install or npm install.
+ * 2. Either create a .env file or directly set a environment variable OPENAI_API_KEY.
+ * 3. Run the main script node index.js:
+ * - if no argument is provided, it will ask the default question: "What is this article about? Can you give me 3 facts about it?"
+ * - optionally you can provide your question by node index.js "your question here"
+ *
+ * Thanks,
+ * Calvin C. Chan
+ */
+
+import dotenv from "dotenv";
+import { RetrievalQAChain } from "langchain/chains";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
+export default async function main(question: string) {
+  const env = process.env;
+  const loader = new CSVLoader("students.csv");
+  const docs = await loader.loadAndSplit();
+  const store = await MemoryVectorStore.fromDocuments(
+    docs,
+    new OpenAIEmbeddings({ openAIApiKey: env.OPENAI_API_KEY })
+  );
+
+  const model = new OpenAI({ openAIApiKey: env.OPENAI_API_KEY });
+  const template = `Use the following student table to answer the question at the end. The student table has 4 columns:
+  1. firstname: string
+  2. gender: M for male and F for female
+  3. division: an integer from 1-10 indicating the homeroom of the student
+  4. lastname: string.
+  Always respond in JSON format as an array of object {{"firstname": "string", "lastname": "string", gender: "string", divison: "integer"}}
+  Student Table:
+  {context}
+  Question: {question}
+  Answer:`;
+  const chain = RetrievalQAChain.fromLLM(model, store.asRetriever(), {
+    prompt: PromptTemplate.fromTemplate(template),
+  });
+
+  const res = await chain.call({
+    query: question,
+  });
+  console.log("Answer:");
+  console.log(res.text);
+}
+
+(async () => {
+  dotenv.config();
+  await main(
+    "List all the girls in division 10, ordered by name in alphabetical order."
+  ).finally(() => {
+    console.log("✨ Done");
+  });
+})();
