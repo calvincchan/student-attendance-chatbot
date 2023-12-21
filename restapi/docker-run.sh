53f3a44d17d9a4e9a2cf84713aa64b940a46a062
Calvin C Chan <hello@calvin-c.com>
2023-12-21 12:52:04 -0800

    chore: improve docker run script

--- restapi/docker-run.sh
+++ restapi/docker-run.sh
@@ -1,2 +1,6 @@
-docker build -t restapi .
-docker run -p 3001:3001 -d restapi
+CONTAINER="restapi"
+IMAGE="restapi"
+PORT="3001"
+docker build -t $IMAGE .
+docker rm -f $CONTAINER
+docker run -p $PORT:$PORT -d --name $CONTAINER $IMAGE
