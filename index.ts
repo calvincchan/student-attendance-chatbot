779f6868afaf90b36d317ab59a3981b13c5f574d
Calvin C Chan <hello@calvin-c.com>
2023-12-24 09:40:44 -0800

    chore: createOpenAPIChain to ask a question

--- index.ts
+++ index.ts
@@ -2,13 +2,16 @@ import "dotenv/config";
 import { createOpenAPIChain } from "langchain/chains";
 
 export default async function main(question: string) {
-  // const openapi = await fetchOpenApi();
-  const chain = await createOpenAPIChain("http://localhost:3001/openapi.yaml");
+  const chain = await createOpenAPIChain("http://localhost:3001/openapi.yaml", {
+    verbose: true,
+  });
   const result = await chain.run(question);
 
   console.log({ result });
 }
 
 (async () => {
-  await main(`find all students in homeroom 7. How many students are there?`);
+  await main(
+    `Use the /students endpoint to find all students in homeroom 1, and then use the /attendances endpoint to mark those students as present on 2023-12-23`
+  );
 })();
