85cdd42d841faeb3f52f5e291ff2cac082a947be
Calvin C Chan <hello@calvin-c.com>
2023-12-27 21:37:15 -0800

    chore: give examples, forward tool calls to actual js functions

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as dayjs from "dayjs";
 import "dotenv/config";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { BaseMessage, FunctionMessage } from "langchain/dist/schema";
+import { BaseMessage, HumanMessage, SystemMessage } from "langchain/schema";
 import { OpenAI } from "openai";
 import * as readline from "readline";
 import { z } from "zod";
@@ -29,11 +29,16 @@ const toolSchema: OpenAI.ChatCompletionTool[] = [
       parameters: zodToJsonSchema(
         z.object({
           homeroom: z.string().optional().describe("Filter by homeroom"),
-          isPresent: z
-            .boolean()
-            .optional()
-            .describe("Filter by present status"),
+          present: z.boolean().optional().describe("Filter by present status"),
           studentId: z.string().optional().describe("Filter by student ID"),
+          fromDate: z
+            .string()
+            .optional()
+            .describe("Filter by from date: YYYY-MM-DD"),
+          toDate: z
+            .string()
+            .optional()
+            .describe("Filter by to date: YYYY-MM-DD"),
         })
       ),
     },
@@ -54,10 +59,11 @@ const toolSchema: OpenAI.ChatCompletionTool[] = [
 ];
 
 /** Get a list of students with optional filters */
-async function findStudents(
-  homeroom: string = "1",
-  isPresent?: boolean
-): Promise<IStudent[]> {
+async function findStudents(args: {
+  homeroom?: string;
+  isPresent?: boolean;
+}): Promise<IStudent[]> {
+  const { homeroom } = args;
   return [
     { id: "1001", homeroom, name: "John" },
     { id: "1002", homeroom, name: "Tom" },
@@ -65,18 +71,39 @@ async function findStudents(
   ];
 }
 
-async function setAllPresentByHomeroom(homeroom: string, date: string) {
+async function setAllPresentByHomeroom(args: {
+  homeroom: string;
+  date: string;
+}) {
+  const { homeroom, date } = args;
   console.log(
     `Mark all students in homeroom ${homeroom} as present on ${date}`
   );
 }
 
+/** Process response from OpenAI, either print the response or call a tool. */
 async function processResponse(response: BaseMessage) {
-  const functionResponse = response as FunctionMessage;
-  console.log("🔥", functionResponse);
+  // console.log(JSON.stringify(response, null, 2));
   if (response.content) {
     console.log("✨", response.content);
     return;
+  }
+  if (response.additional_kwargs?.tool_calls) {
+    const toolCalls = response.additional_kwargs.tool_calls;
+    for (const toolCall of toolCalls) {
+      const { function: func } = toolCall;
+      console.log(
+        "🚀 ~ file: index.ts:89 ~ processResponse ~ toolCall:",
+        toolCall
+      );
+      const args = JSON.parse(func.arguments);
+      if (func.name === "findStudents") {
+        const students = await findStudents(args);
+        console.log("✨", students);
+      } else if (func.name === "setAllPresentByHomeroom") {
+        await setAllPresentByHomeroom(args);
+      }
+    }
   }
 }
 
@@ -93,29 +120,35 @@ export default async function main() {
     tool_choice: "auto",
   });
 
-  // const chain = new LLMChain({
-  //   llm: model,
-  //   prompt,
-  // });
-
-  // /** Init API request */
-  // const result = await chain.call({
-  //   homeroom: defaultHomeroom,
-  //   date: defaultDate,
-  //   text: `Please mark all students in my class as present today.`,
-  // });
+  /** Init message. */
   const result = await model.invoke([
-    [
-      "system",
-      `You are a helpful assistance that help a human teacher to interact with a student attendance management system with function calls. Use the following context to help you understand the conversation. Address the human user as ${defaultTeacherName}.
-    ----------
-    Teacher homeroom: ${defaultHomeroom}
-    Today's date: ${defaultDate}
-    `,
-    ],
-    ["human", `Hello, What's my name and today's date?`],
+    new SystemMessage(
+      `The following is a friendly conversation between a human teacher and an AI. The AI helps the teacher to manage student attendance via function calls.
+--------
+Context:
+- Address the human user as ${defaultTeacherName}.
+- The default homeroom is ${defaultHomeroom}.
+- Today's date is ${defaultDate}.
+- If the AI does not know the answer to a question, it truthfully says it does not know.
+--------
+Examples:
+- Teacher: Mark all students as present, except for John who has fever.
+  function name: "setAllPresentByHomeroom", args: {{homeroom: "${defaultHomeroom}", date: "${defaultDate}"}}
+  function name: "setAttendance", args: {{name: "John", date: "${defaultDate}", present: false, reason: "fever"}}
+- Teacher: Tom is sick today. The rest of the class is present.
+  function name: "setAllPresentByHomeroom", args: {{homeroom: "${defaultHomeroom}", date: "${defaultDate}"}}
+  function name: "setAttendance", args: {{name: "Tom", date: "${defaultDate}", present: false, reason: "sick"}}
+- Teacher: Who was absent in homeroom 7 last week?
+  function name: "getAttendances", args: {{homeroom: "7", present: false, fromDate: "${dayjs(
+    defaultDate
+  )
+    .subtract(7, "days")
+    .format("YYYY-MM-DD")}", toDate: "${defaultDate}"}}
+`
+    ),
+    new HumanMessage(`Hello, What's my name and today's date?`),
   ]);
-  console.log(JSON.stringify(result, null, 2));
+  processResponse(result);
 
   /**
    * Create an interactive prompt user interface.
@@ -126,7 +159,7 @@ export default async function main() {
   });
   userInterface.prompt();
   userInterface.on("line", async (text) => {
-    const response = await model.invoke(["human", text]);
+    const response = await model.invoke([new HumanMessage(text)]);
     await processResponse(response);
     userInterface.prompt();
   });
